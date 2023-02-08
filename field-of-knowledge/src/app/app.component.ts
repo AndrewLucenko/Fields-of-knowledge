5cb546101bfb555cf94f38bf61e6535547a94fee
TheFikusF <sasha.naykus@gmail.com>
2023-02-08 22:48:32 +0200

    Initialized window

--- field-of-knowledge/src/app/app.component.ts
+++ field-of-knowledge/src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as PIXI from 'pixi.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'field-of-knowledge';
+export class AppComponent implements OnInit {
+  private app: PIXI.Application = new PIXI.Application({width: 640, height: 360});
+  
+  ngOnInit() {
+    document.body.appendChild(this.app.view as HTMLCanvasElement);
+    const style = new PIXI.TextStyle({
+      fontFamily: 'Arial',
+      fontSize: 36,
+      fontWeight: 'bold',
+      fill: ['#ffffff'], 
+      stroke: '#4a1850',
+    });
+
+    const basicText = new PIXI.Text('привет мир я гуль...', style);
+    basicText.x = 10;
+    basicText.y = 0;
+
+    var time : number = 0;
+
+    this.app.ticker.add((delta) => {
+      time += delta;
+      basicText.x = 100 + Math.sin(time * delta * 0.01) * 100;
+    });
+
+    this.app.stage.addChild(basicText);
+  }
 }
+
