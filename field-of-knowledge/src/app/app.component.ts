501ade7ef677355662f389aaee4a2f8ab4fbc61f
TheFikusF <sasha.naykus@gmail.com>
2023-03-01 19:55:16 +0200

    pixi kal

--- field-of-knowledge/src/app/app.component.ts
+++ field-of-knowledge/src/app/app.component.ts
@@ -7,10 +7,12 @@ import * as PIXI from 'pixi.js';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  private app: PIXI.Application = new PIXI.Application({width: 640, height: 360});
+  private app: PIXI.Application = new PIXI.Application({resizeTo: document.getElementById("application") as HTMLCanvasElement});
   
   ngOnInit() {
-    document.body.appendChild(this.app.view as HTMLCanvasElement);
+    document.getElementById("application")!.appendChild(this.app.view as HTMLCanvasElement);
+    console.log(document.body.getElementsByClassName("application")[0]);
+
     const style = new PIXI.TextStyle({
       fontFamily: 'Arial',
       fontSize: 36,
@@ -23,24 +25,51 @@ export class AppComponent implements OnInit {
     basicText.x = 10;
     basicText.y = 0;
 
-    const button = new PIXI.Text('da', style);
-    button.x = 10;
-    button.y = 100;
-    button.addListener('pointerdown', () => {
-      button.text = 'net';
-      console.log('working');
-    });
-
     var time : number = 0;
 
+    let sprite = PIXI.Sprite.from("../assets/no_borders.svg");
+    console.log(sprite.width);
+    console.log(sprite.height);
+    sprite.x = 10
+    sprite.y = 80
+
+    sprite.interactive = true;
+
+    sprite.on('pointerdown', this.onClick);
+    console.log(sprite.width);
+    console.log(sprite.height);
+
+    this.app.stage.addChild(sprite);
     this.app.stage.addChild(basicText);
-    this.app.stage.addChild(button);
 
     this.app.ticker.add((delta) => {
       time += delta;
       basicText.x = 100 + Math.sin(time * 0.01) * 100;
     });
 
+    setTimeout(() => {
+      console.log("dispatched")
+      const eventInitDict = {
+        view: window,
+        bubbles: true,
+        cancelable: true,
+        button: 0,
+        buttons: 1,
+        isPrimary: true,
+        pointerType: 'mouse',
+        clientX: window.innerWidth + 1,
+        clientY: window.innerHeight + 1,
+      };
+    
+      this.app.renderer.view.dispatchEvent( new PointerEvent( 'pointerdown', eventInitDict ) );
+      this.app.renderer.view.dispatchEvent( new PointerEvent( 'pointerup', eventInitDict ) );
+      
+    }, 1000)
+  }
+
+  onClick() {
+
+    console.log('hui')
   }
 }
 
